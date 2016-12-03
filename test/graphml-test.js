6c0fd92199bc60f81d816371c18f01a1f70a81b7
Renat Idrisov <ren@ngs.ru>
2016-12-03 23:22:52 +0100

    Tests for all available Sisal programs

--- test/graphml-test.js
+++ test/graphml-test.js
@@ -3,7 +3,7 @@ var assert = buster.referee.assert;
 var fs = require("fs");
 
 function checkDir(path) {
-    var exclude = ["complex"];
+    var exclude = ["nothing_yet_to_exclude"];
     var data = fs.readdirSync(path);
     for (var i = 0; i<data.length; i++) {
         if (fs.lstatSync(path+data[i]).isDirectory()) {
