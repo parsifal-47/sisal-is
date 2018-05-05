b9b286076644b6da9d2e823bb9263212f4df641f
Renat Idrisov <ren@ngs.ru>
2018-05-05 22:13:43 +0200

    GraphML preparations

---
+++ src/graphml/generate.ts
@@ -0,0 +1,36 @@
+export function makeNode(id: string, name: string, inPorts: number,
+                         outPorts: number): string {
+  const genFunction = (prefix: string, quantity: number) => {
+    return (Array.from(new Array(quantity),
+       (val, index) => "<port name=\"" + prefix + String(index) + "\">").join(""));
+  };
+
+  return "<node id=\"" + id + "\">\n" +
+         "<data key=\"type\">" + name + "</data>" +
+         genFunction("in", inPorts) +
+         genFunction("out", outPorts) +
+         "</node>";
+}
+
+export function makeEdge(idFrom: string, portFrom: number,
+                         idTo: string, portTo: number,
+                         inner: boolean = false): string {
+  return "<edge source=\"" + idFrom + "\" target=\"" + idTo +
+        "\" sourceport=\"out" + portFrom +
+        "\" targetport=\"" + (inner ? "out" : "in") + portTo + "\"/>";
+}
+
+export function makeDocument(contents: string): string {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <graphml xmlns="http://graphml.graphdrawing.org/xmlns"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+          http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+    <key id="type" for="node" attr.name="nodetype" attr.type="string" />
+` + contents + `
+</graphml>`;
+}
+
+export function makeGraph(id: string, contents: string): string {
+  return "<graph id=\"" + id + "\" edgedefault=\"directed\">" + contents + "</graph>";
+}
--- src/ir1/nodes/node.ts
+++ src/ir1/nodes/node.ts
@@ -1,16 +1,24 @@
 import { Port } from "../ports/port";
 
 export class Node {
+  private static lastId: number = 0;
+
   public name: string;
   public location: string;
   public outPorts: Port[];
   public inPorts: Port[];
+  public siblings: Node[];
+  public id: number;
 
   constructor(name: string) {
+    Node.lastId++;
+
     this.name = name;
     this.location = "unknown";
     this.outPorts = [];
     this.inPorts = [];
+    this.siblings = [];
+    this.id = Node.lastId;
   }
 
   public requestPorts(portNum: number): number {
@@ -22,7 +30,16 @@ export class Node {
       if (node.requestPorts(1) !== 1) {
         throw new Error("Each part should produce exactly one output");
       }
+      this.siblings.push(node);
       this.inPorts.push(node.outPorts[0]);
     }
+  }
+
+  public getInEdges(): Array<[Node, number]> {
+    const edges: Array<[Node, number]> = [];
+    for (const node of this.siblings) {
+      edges.push([node, 0]);
+    }
+    return edges;
   }
 }
